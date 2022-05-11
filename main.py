c02245f8e0c722b9c5cbd173c554f77864d51d03
Erik <erikleos23@gmail.com>
2022-05-11 16:59:16 -0500

    Base 0.11v

--- main.py
+++ main.py
@@ -14,18 +14,32 @@ clock = pygame.time.Clock()
 
 BackGround_Surf = pygame.image.load("graphics/BackGround_1.png").convert()
 
-Flapp_Gravity = -10
 
-Flapp_Surf = pygame.image.load("graphics/FlappE_1.png").convert_alpha()
-Flapp_Surf = pygame.transform.scale(Flapp_Surf, (51, 36))
-Flapp_Rect = Flapp_Surf.get_rect(midbottom=(50, 50))
-Flapp_Sprites = ["graphics/FlappE_1.png", "graphics/FlappE_2.png", "graphics/FlappE_3.png"]
-Flapp_Sprites_Index = 0
+class FlappEPlayer:
+    def __init__(self):
+        self.Flapp_Gravity = -10
+        self.Flapp_Sprites = []
+        self.Flapp_Sprites.append(pygame.image.load("graphics/FlappE_1.png").convert_alpha())
+        self.Flapp_Sprites.append(pygame.image.load("graphics/FlappE_2.png").convert_alpha())
+        self.Flapp_Sprites.append(pygame.image.load("graphics/FlappE_3.png").convert_alpha())
+        self.Flapp_Sprites.append(pygame.image.load("graphics/FlappE_2.png").convert_alpha())
+        self.Flapp_SprIndex = 0
 
-def Flapp_Anim(Flapp_Sprites_Index):
+        self.Flapp_Surf = self.Flapp_Sprites[self.Flapp_SprIndex]
+        self.Flapp_Surf = pygame.transform.scale(self.Flapp_Surf, (51, 36))
+        self.Flapp_Rect = self.Flapp_Surf.get_rect(midbottom=((screen.get_width() / 2) - (self.Flapp_Surf.get_width() / 2) + 20, 100))
 
-    Flapp_Surf = pygame.image.load(Flapp_Sprites[Flapp_Sprites_Index]).convert_alpha()
-    Flapp_Sprites_Index += 1
+    def updateSprite(self):
+        self.Flapp_SprIndex += 0.15
+
+        if self.Flapp_SprIndex >= len(self.Flapp_Sprites):
+            self.Flapp_SprIndex = 0
+
+        self.Flapp_Surf = self.Flapp_Sprites[int(self.Flapp_SprIndex)]
+        self.Flapp_Surf = pygame.transform.scale(self.Flapp_Surf, (51, 36))
+
+
+Flapp = FlappEPlayer()
 
 while True:
     for event in pygame.event.get():
@@ -34,21 +48,22 @@ while True:
             exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                Flapp_Gravity = 7
+                Flapp.Flapp_Gravity = 7
 
-    screen.blit(BackGround_Surf, (0,0))
+    screen.blit(BackGround_Surf, (0, 0))
 
-    screen.blit(Flapp_Surf, Flapp_Rect)
-    Flapp_Rect.y -= Flapp_Gravity
-    Flapp_Gravity -= 0.35
+    screen.blit(Flapp.Flapp_Surf, Flapp.Flapp_Rect)
+    Flapp.Flapp_Rect.y -= Flapp.Flapp_Gravity
+    Flapp.Flapp_Gravity -= 0.35
 
-    pygame.set
+    if Flapp.Flapp_Gravity < -10:
+        Flapp.Flapp_Gravity = -10
 
-    if Flapp_Gravity < -10:
-        Flapp_Gravity = -10
+    if Flapp.Flapp_Rect.y > 350:
+        Flapp.Flapp_Rect.y = 350
 
-    if Flapp_Rect.y > 350:
-        Flapp_Rect.y = 350
+    Flapp.updateSprite()
+
     pygame.display.update()
     clock.tick(60)
 
